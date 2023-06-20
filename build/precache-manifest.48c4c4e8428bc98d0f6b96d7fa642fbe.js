65b5617d94cbea26e7e64819927ad0ab4bf6f15d
Guilherme Blanco Gomes <guilhermeblancogomes@gmail.com>
2023-06-20 12:11:19 -0300

    deploy

---
+++ build/precache-manifest.48c4c4e8428bc98d0f6b96d7fa642fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f1cb9eb537a74c6559956b8c6f0d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7c7e68cd7cfb75c9450",
+    "url": "/static/css/2.c0e4f2a0.chunk.css"
+  },
+  {
+    "revision": "9dc4fabc56871df1bbe3",
+    "url": "/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "f7c7e68cd7cfb75c9450",
+    "url": "/static/js/2.dc4fb4fb.chunk.js"
+  },
+  {
+    "revision": "9dc4fabc56871df1bbe3",
+    "url": "/static/js/main.d03f8e55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
